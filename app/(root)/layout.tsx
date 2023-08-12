bf6d630b660b17029522290a707744520f935d9b
Prathibha Ratnayake <prsthibha@gmail.com>
2023-08-12 12:15:02 +0530

    Add initial assets and navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,22 +1,29 @@
-import '../globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import Header from '@/components/header';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '../globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Prathibha | Personal Portfolio',
   description: 'Prathibha is a Fullstack developer with 3 years of experience',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-gray-50 text-gray-950`}>{children}</body>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+        <div className='bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] -z-10' />
+
+        <div className='bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-33rem] 2xl:left-[-5rem] -z-10' />
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
